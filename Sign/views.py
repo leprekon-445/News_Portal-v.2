79a30166354296c7e3b019afb2e7cc7325293294
Евгений <146889684+leprekon-445@users.noreply.github.com>
2025-06-04 14:10:39 +0300

    Create views.py

---
+++ Sign/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import Group
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from NewsPortal.models import Author
+
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = '/'    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_premium'] = not self.request.user.groups.filter(name = 'authors').exists()        
+        return context
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    if not Author.objects.filter(user = user):
+            Author.objects.create(user=user)
+    premium_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
